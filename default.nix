f061222ee6d88cff2ab56106ec8994729377559e
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-01-27 19:36:09 +0100

    finalhe: init at 1.92
    The cmake file patch is included to add an install target.

--- default.nix
+++ default.nix
@@ -7,5 +7,6 @@ let
     gnomeExtensions = { no-title-bar = callPackage ./pkgs/no-title-bar { }; };
     libvitamtp = callPackage ./pkgs/libvitamtp { };
     qcma = callPackage ./pkgs/qcma { };
+    finalhe = callPackage ./pkgs/finalhe { };
   };
 in self
---
+++ pkgs/finalhe/default.nix
@@ -0,0 +1,38 @@
+{ stdenv, fetchFromGitHub, cmake, qt5, libdeflate, libusb, libxml2, pkgconfig
+, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  pname = "finalhe";
+  version = "1.92";
+
+  src = fetchFromGitHub {
+    owner = "soarqin";
+    repo = "finalhe";
+    rev = "v${version}";
+    sha256 = "0i9y908lbpkk3z2i8sp3q39rmsbmpa3y92jwrnzhc5f4hah8iqrg";
+  };
+
+  nativeBuildInputs = [
+    pkgconfig
+    cmake
+    libxml2
+    libusb
+    libdeflate
+    qt5.qtbase
+    qt5.qttools
+    makeWrapper
+  ];
+
+  patches = [ ./cmake.patch ];
+
+  postInstall = ''
+    wrapProgram $out/bin/FinalHE \
+      --prefix QT_PLUGIN_PATH : "${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "A tool to push the h-encore explot for PS Vita/PS TV automatically";
+    homepage = "https://github.com/soarqin/finalhe"
+    license = licenses.gpl3;
+  };
+}
