b4f09ab56129f808a94b8e7f196ba9b062b63c65
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-05-21 11:51:38 +0200

    no-title-bar: revision bump, patch update

--- pkgs/no-title-bar/default.nix
+++ pkgs/no-title-bar/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-shell-extension-no-title-bar-unstable";
-  version = "62f1c4e32fd982b48fdc3161674a0abb15ee20e0";
+  version = "4115905e1d3df51072a2c6b173a471667181b31a";
 
   src = fetchFromGitHub {
     owner = "poehlerj";
     repo = "no-title-bar";
     rev = "${version}";
-    sha256 = "1jq12s3l30fnm2djs2p21mkzf463r1lp4lmmhvbj2xx8lpcmks99";
+    sha256 = "0hcbbfapk76lr8yajacx59cyzs2c1dnccf8fq3gv3zk1z8jfqb1h";
   };
 
   nativeBuildInputs = [ glib gettext ];
